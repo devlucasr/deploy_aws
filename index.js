2107f0d072344a8fe752df0540b6c1c6a8c9684a
Lucas Ribeiro <lucas.ribeiro@tecnospeed.com.br>
2022-11-22 21:30:03 -0300

    Primeiro Envio

---
+++ index.js
@@ -0,0 +1,33 @@
+const { request, response } = require('express');
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express();
+app.use(bodyParser.json())
+const port = 8000
+
+
+app.get('/',(request, response)=>{
+    return response.end('Bem vindo ao Super Servidor')
+});
+
+app.get('/somar',(request, response)=>{
+    const body = request.body
+    
+    const soma = body.n1 + body.n2
+    
+    return response.send(`O resultado da soma é ${soma}`)
+
+})
+
+app.get('/cliente',(request, response)=>{
+    return response.json(
+    { 
+        "Cliente": "Lucas",
+        "Cidade":"Aracatuba",
+        "Idade": 21
+    })
+});
+
+app.listen(port, ()=>{
+    console.log('Servidor Iniciado com Sucesso')
+})
